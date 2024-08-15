143e29abaff4649c9d280eca7eed32421a739e80
cardoso-thiago <thiago.cardoso33@gmail.com>
2024-08-15 18:21:36 -0300

    fix: Teste com alteração no repo externo

--- src/main/java/br/com/cardoso/controller/PersonController.java
+++ src/main/java/br/com/cardoso/controller/PersonController.java
@@ -19,7 +19,7 @@ public class PersonController {
     // highlight-start
     @GetMapping("/person")
     public ResponseEntity<Person> test() {
-        System.out.println("Teste para modificação do conteúdo de maneira não sincronizada")
+        System.out.println("Teste para modificação do conteúdo de maneira não sincronizada");
         return ResponseEntity.ok(personService.getPerson()) ;
     }
     // highlight-end
